57ce157b6960260b7b75a72c51169c75345ee804
harryzawg <harry@zawg.cloud>
2024-07-18 01:43:30 -0400

    Update cmd.py

--- src/cmd.py
+++ src/cmd.py
@@ -228,13 +228,13 @@ class NewInteractiveShell:
             except (EOFError, KeyboardInterrupt):
                 return
 
-   async def start(self, args):
-        try:
-            await self.command_parser(' '.join(args))
-        except (EOFError, KeyboardInterrupt):
-            return
-        finally:
-            logger.info("Existing shell")
+     async def start(self, args):
+         try:
+             await self.command_parser(' '.join(args))
+         except (EOFError, KeyboardInterrupt):
+             return
+         finally:
+             logger.info("Existing shell")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
