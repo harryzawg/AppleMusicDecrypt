5bc84ee876728f7fe8ac49975dd8a2647cfb7524
harryzawg <harry@zawg.cloud>
2024-07-18 02:24:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from src.cmd import NewInteractiveShell
 
 if __name__ == '__main__':
     if sys.platform in ('win32', 'cygwin', 'cli'):
-        import winloop
-        winloop.install()
+        try:
+            import uvloop
+            uvloop.install()
+        except ImportError:
+            print("uvloop not installed. Falling back to default asyncio.")
     else:
         import uvloop
         uvloop.install()
+    
     loop = asyncio.get_event_loop()
     cmd = NewInteractiveShell(loop)
     try:
